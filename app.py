3a290ffbd0cd5bc4536bc8d85e06e117689149c8
tsesunny1003 <tsesunny1003@gmail.com>
2025-11-21 10:20:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,3 +75,26 @@ business_insights = [
 for insight in business_insights:
     placeholder.write(insight)
     time.sleep(2)
+
+
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(rng(0).standard_normal((20,3)), columns=["a", "b", "c"])
+
+st.area_chart(df)
+
+
+
+
+
+option = st.selection(
+    "How would you like to be contacted?", 
+    ["Email?",
+    "Home phone",
+    "Mobile phone"],
+)
+
+
+st.write("You selected:", option)
