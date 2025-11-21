f347b6c0be974408defdb133b8fcdac2b8406314
tsesunny1003 <tsesunny1003@gmail.com>
2025-11-21 09:37:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ with tab2:
         "Quick delivery and excellent support."
     ]
     for feedback in customer_feedback:
-        st.write(f"# {feedback}")
+        st.write(f"enumerate {feedback}")
 with tab3:
     st.write("Content for Market Trends")
     market_trends = {
